8984f7a9075216acebee6992966605096b6ef92b
Fernando <valdesreciof@gmail.com>
2023-05-07 23:16:44 -0400

    ignore pycache

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db 
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -15,6 +16,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in sucessfully!', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect User/Pasword, try again.', category='error')               
